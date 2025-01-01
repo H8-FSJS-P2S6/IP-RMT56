e73f20a46aa11491409c12746604a1f9e201b85d
Syabina Aisyah <syabinaaisyahputri.mipa3@gmail.com>
2025-01-02 00:36:05 +0700

    server: user

--- server/Skeleton.md
+++ server/Skeleton.md
@@ -1,4 +1,4 @@
-npx sequelize-cli model:generate --name User --attributes email:string,password:string
+npx sequelize-cli model:generate --name User --attributes fullName:string,email:string,password:string
 
 npx sequelize-cli model:generate --name Pokemon --attributes name:string,image:string,role:string,primary_attribute:string,abilities:jsonb,health:integer,mana:integer,strength:integer,agility:integer,intelligence:integer,lore:text,attack_range:integer,movement_speed:integer
 
---
+++ server/models/user.js
@@ -0,0 +1,60 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+const { hashPass } = require('../helpers/bcrypt')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.MyPokemon, {
+        foreignKey: "userId",
+        as: "pokemons"
+      });
+    }
+  }
+  User.init({
+    fullName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {
+          msg: "Email is required"
+        },
+        notEmpty: {
+          msg: "Email is required"
+        },
+        isEmail: true, 
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Password is required"
+        },
+        notEmpty: {
+          msg: "Password is required"
+        }
+      }
+    },
+  }, {
+    hooks: {
+      beforeCreate(user) {
+        user.password = hashPass(user.password)
+      }
+    },
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
