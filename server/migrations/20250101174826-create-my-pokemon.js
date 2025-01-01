016e1801bf953496a1bace20a59b7629a096cb87
Syabina Aisyah <syabinaaisyahputri.mipa3@gmail.com>
2025-01-02 01:09:02 +0700

    server: pokemon & myPokemon

--- server/migrations/20250101174826-create-my-pokemon.js
+++ server/migrations/20250101174826-create-my-pokemon.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "Monsters",
+          model: "Pokemons",
           key: "id",
         },
         onDelete: "CASCADE",
